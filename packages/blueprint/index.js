0be245e59843bf2d8bf6d2cf82dc42d77e027705
alecsgone <alexlopezarroyave@gmail.com>
2018-03-06 16:18:09 -0500

    add new example

--- packages/blueprint/index.js
+++ packages/blueprint/index.js
@@ -1,11 +1,20 @@
 import dom from 'jsx-render'
 import Headline from './headline'
 
+const items = [1,2,3,4]
 function Main() {
   return (
     <div>
       <Headline />
       <p>Lorem ipsum</p>
+      <ul>
+        <li><a href="">anchor</a></li>
+        <li>2</li>
+        <li><a href="">anchor2</a> More</li>
+      </ul>
+      <ol>
+        {items.map(item => <li>{item}</li>)}
+      </ol>
     </div>
   )
 }
